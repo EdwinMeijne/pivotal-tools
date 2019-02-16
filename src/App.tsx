708ada093325affad7954f2ee4d36108124d99f3
edwinmeijne <git@edwinmeijne.nl>
2019-02-16 16:50:24 +0100

    * intermediate commit - start refactoring to abstract stuff away from component

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,46 @@ import React, {useState} from 'react';
 import './App.css';
 import PivotalTokenInput, {PivotalState} from "./PivotalTokenInput";
 
+interface PivotalAccount {
+    id: number,
+    name: string,
+}
+
+interface PivotalProject {
+    project_id: number,
+    project_name: string,
+    project_color: string,
+    favorite: true,
+    role: string,
+}
+
+interface PivotalUser {
+    name: string,
+    accounts: PivotalAccount[],
+    projects: PivotalProject[],
+    email: string,
+}
+
+const initialState: {
+    xtoken?: string,
+    user?: PivotalUser
+} = {};
 
 export function App() {
-  const [pivotalState, setPivotalData] = useState({ xtoken: '', user: {} });
+    const [pivotalState, setPivotalData] = useState(initialState);
 
-  if (!pivotalState.xtoken) {
-    return (
-      <main>
-        <h1>Login</h1>
-        <PivotalTokenInput setPivotalData={setPivotalData}/>
-      </main>
-    );
-  } else {
-    return (
-      <main>
-        <h1>Login with user:</h1>
-        <pre>{pivotalState.xtoken}</pre>
-      </main>
-    );
-  }
+    if (!pivotalState.xtoken) {
+        return (
+            <main>
+                <h1>Login</h1>
+                <PivotalTokenInput setPivotalData={setPivotalData}/>
+            </main>
+        );
+    } else {
+        return (
+            <main>
+                <h1>Welcome, {pivotalState.user.name}</h1>
+            </main>
+        );
+    }
 }
